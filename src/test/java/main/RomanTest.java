6ee851fa856e2837d0b83d14a086551abdf4e7cd
Jeremy Boy <github@jboy.eu>
2020-11-07 00:42:22 +0100

    add test case for toRoman(1) == 'I'

--- src/test/java/main/RomanTest.java
+++ src/test/java/main/RomanTest.java
@@ -3,6 +3,14 @@ package main;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class RomanTest {
-    
+
+    @Test
+    public void testI() {
+        Roman roman = new Roman();
+        assertEquals(roman.toRoman(1), "I");
+    }
+
 }

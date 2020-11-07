59ba48f20f09786b9f1202d0f547ca1b9e54d5fc
Jeremy Boy <github@jboy.eu>
2020-11-07 01:30:10 +0100

    refactor tests, use HashMap with expected values

--- src/test/java/main/RomanTest.java
+++ src/test/java/main/RomanTest.java
@@ -3,37 +3,33 @@ package main;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class RomanTest {
 
     @Test
-    public void testI() {
+    public void testAllCases() {
+        Map<Integer, String> m = new HashMap<>();
+        m.put(1, "I");
+        m.put(2, "II");
+        m.put(3, "III");
+        m.put(4, "IV");
+        m.put(5, "V");
+        m.put(6, "VI");
+        m.put(7, "VII");
+        m.put(8, "VIII");
+        m.put(9, "IX");
+        m.put(10, "X");;
+        m.put(14, "XIV");
+        m.put(15, "XV");
+        m.put(20, "XX");
         Roman roman = new Roman();
-        assertEquals(roman.toRoman(1), "I");
-    }
-
-    @Test
-    public void testII() {
-        Roman roman = new Roman();
-        assertEquals(roman.toRoman(2), "II");
-    }
-
-    @Test
-    public void testIII() {
-        Roman roman = new Roman();
-        assertEquals(roman.toRoman(3), "III");
-    }
-
-    @Test
-    public void testIV() {
-        Roman roman = new Roman();
-        assertEquals(roman.toRoman(4), "IV");
-    }
-
-    @Test
-    public void testV() {
-        Roman roman = new Roman();
-        assertEquals(roman.toRoman(5), "V");
+        for (Integer arabicNumber: m.keySet()) {
+            String romanNumber = m.get(arabicNumber);
+            assertEquals(romanNumber, roman.toRoman(arabicNumber));
+        }
     }
 }
